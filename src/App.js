cdcbded5077ad81c25b9d87970e0ffe23fe19649
John Money <jemoney@protonmail.com>
2019-05-07 23:27:13 -0400

    initial content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './trellisly.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="Trellisly" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Growing the ridiculously easy to use account marketing platform.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
