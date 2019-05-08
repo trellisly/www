25a5792ba812e00c98eb866e77ba750e2e3756e1
John Money <jemoney@protonmail.com>
2019-05-07 22:58:44 -0400

    Updates

---
+++ precache-manifest.2031cb773eff769cbf578d72907cee22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cdba7c462d11e9468da0798356ff30",
+    "url": "/www/index.html"
+  },
+  {
+    "revision": "05ab8c46c0c99a3ebc81",
+    "url": "/www/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/www/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "05ab8c46c0c99a3ebc81",
+    "url": "/www/static/js/main.6964cd4a.chunk.js"
+  },
+  {
+    "revision": "62fa8478bae7b6596d97",
+    "url": "/www/static/js/runtime~main.d6b53820.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/www/static/media/logo.5d5d9eef.svg"
+  }
+]);
