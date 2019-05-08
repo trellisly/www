6d6c12ee8b4f52c5677b82032621ce5f90c8c696
John Money <jemoney@protonmail.com>
2019-05-08 01:38:42 -0400

    Updates

---
+++ precache-manifest.7f6b0d106447c24a8ff961bd6779c5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dede3cefd5bd921e34a9630af4df5f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0852c2686b7a688d7a1f",
+    "url": "/static/css/main.8bf58a82.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0852c2686b7a688d7a1f",
+    "url": "/static/js/main.6667c826.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d63b29e8dadec0e823b7f00f54dc38fa",
+    "url": "/static/media/trellisly.d63b29e8.png"
+  }
+]);
